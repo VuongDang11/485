cc567726464e2028ffd62a440c7ea6645943b7fd
Vuong Dang <vdang1128@gmail.com>
2023-01-15 20:00:50 -0800

    Boxes layout for Plan Page

--- 485/Sprint1/plan.php
+++ 485/Sprint1/plan.php
@@ -4,43 +4,151 @@
       <meta charset="UTF-8">
       <title>Title</title>
       <script src="scripts/token.js"></script>
-      <link rel="stylesheet" href="styles/index.css"
+        <link rel="stylesheet" href="styles/plan.css">
   </head>
   <body>
+<div class="token-generate">
+    <h1 id="quarter">Quarter Classes Planning</h1>
 
+    <?php
 
-  <p>Feature test</p>
-  <div class="container">
-        <div class="fall-plan">
-            Fall
-            <textarea>?</textarea>
-        </div>
+    function IdGenerator($length = 6) {
+        $characters = 'ABCDEFHIJKLMNOPQRSTUVWXYZ2345679';
+        $strlen = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $strlen - 1)];
+        }
+        return $randomString;
+    }
+    echo "Your personal ID: ".IdGenerator();
 
-        <div class="winter-plan">
-        </div>
+    ?>
 
+</div>
+
+  <div class="container my-container">
+            <form>
+                <div class="fall-plan">
+                    <h1>Fall Planning</h1>
+                    <div class="form-group" id="mail">
+                        <label for="studentEmail">Student Email Address</label>
+                         <br>
+                        <input type="email" class="form-control" id="studentEmail" placeholder="@student.greenriver.edu">
+                    </div>
+                    <div class="form-group">
+                        <label for="classTake">How many classes do you want to take</label>
+                        <br>
+                        <select class="form-control" id="classTake">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="class-planning" id="classPlan">Class Planning</label>
+                        <br>
+                        <textarea class="form-control" id="class-planning" rows="15" cols="60"></textarea>
+                    </div>
+
+                    <button type="submit">Save the plan</button>
+                </div>
+            </form>
+            <form>
+                <div class="winter-plan">
+                    <h1>Winter Planning</h1>
+                    <div class="form-group" id="mail">
+                        <label for="studentEmail">Student Email Address</label>
+                        <br>
+                        <input type="email" class="form-control" id="studentEmail" placeholder="@student.greenriver.edu">
+                </div>
+                <div class="form-group">
+                        <label for="classTake">How many classes do you want to take</label>
+                        <br>
+                        <select class="form-control" id="classTake">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        </select>
+                </div>
+            <div class="form-group">
+                <label for="class-planning" id="classPlan">Class Planning</label>
+                <br>
+                <textarea class="form-control" id="class-planning" rows="15" cols="60"></textarea>
+            </div>
+            <button type="submit">Save the plan</button>
+
+            </div>
+            </form>
   </div>
 
 
-  <br>
+  <div class="container my-container-second-row">
+      <form>
+          <div class="spring-plan">
+              <h1>Spring Planning</h1>
+              <div class="form-group" id="mail">
+                  <label for="studentEmail">Student Email Address</label>
+                  <br>
+                  <input type="email" class="form-control" id="studentEmail" placeholder="@student.greenriver.edu">
+              </div>
+              <div class="form-group">
+                  <label for="classTake">How many classes do you want to take</label>
+                  <br>
+                  <select class="form-control" id="classTake">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                  </select>
+              </div>
+              <div class="form-group">
+                  <label for="class-planning" id="classPlan">Class Planning</label>
+                  <br>
+                  <textarea class="form-control" id="class-planning" rows="15" cols="60"></textarea>
+              </div>
 
+              <button type="submit">Save the plan</button>
+          </div>
 
+      </form>
 
-<?php
+      <form>
+          <div class="summer-plan">
+              <h1>Summer Planning</h1>
+              <div class="form-group" id="mail">
+                  <label for="studentEmail">Student Email Address</label>
+                  <br>
+                  <input type="email" class="form-control" id="studentEmail" placeholder="@student.greenriver.edu">
+              </div>
+              <div class="form-group">
+                  <label for="classTake">How many classes do you want to take</label>
+                  <br>
+                  <select class="form-control" id="classTake">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                  </select>
+              </div>
+              <div class="form-group">
+                  <label for="class-planning" id="classPlan">Class Planning</label>
+                  <br>
+                  <textarea class="form-control" id="class-planning" rows="15" cols="60"></textarea>
+              </div>
 
-function IdGenerator($length = 6) {
-    $characters = 'ABCDEFHIJKLMNOPQRSTUVWXYZ2345679';
-    $strlen = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $strlen - 1)];
-    }
-    return $randomString;
-}
-echo "Your personal ID: ".IdGenerator();
+              <button type="submit">Save the plan</button>
+          </div>
 
+      </form>
+  </div>
 
-?>
-    </body>
+  </body>
 
 </html>
