b4743bbe49b0fafbf6a7a1b47552a5f0af3b040c
Vuong Dang <vdang1128@gmail.com>
2023-01-24 21:40:33 -0800

    Adding more features and display data after submit

--- 485/Sprint1/formValues.php
+++ 485/Sprint1/formValues.php
@@ -1,6 +1,3 @@
-<?php
-include ('model/connect.php');
-?>
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
@@ -11,24 +8,10 @@ include ('model/connect.php');
 </head>
 <body>
 <div class="token-generate">
-    <h1 id="quarter">Quarter Classes Planning</h1>
-
-    <?php
-
-    function IdGenerator($length = 6) {
-        $characters = 'ABCDEFHIJKLMNOPQRSTUVWXYZ2345679';
-        $strlen = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $strlen - 1)];
-        }
-        return $randomString;
-    }
-    echo "Your personal ID: ".IdGenerator();
-
-    ?>
+    <h1 id="quarter">Schedule after planning</h1>
 
 </div>
+
 <h3>Student Name</h3>
 <input type="text" class="form-control" id="name" name="name" placeholder="First / Last">
 <br>
@@ -36,13 +19,17 @@ include ('model/connect.php');
 <input type="email" class="form-control" id="email" name="email" placeholder="@student.greenriver.edu">
 
 <div class="container my-container">
-    <form action="formValues.php" method="post">
+    <form action="" method="post">
 
         <div class="fall-plan">
             <h1>Fall Planning</h1>
 
             <div class="form-group">
-                <textarea class="form-control" name="fall" id="fall" rows="15" cols="60"></textarea>
+                <textarea class="form-control" name="fall" id="fall" rows="15" cols="60">
+                    <?php
+                        echo $_POST['fall']
+                    ?>
+                </textarea>
             </div>
         </div>
 
@@ -50,7 +37,12 @@ include ('model/connect.php');
             <h1>Winter Planning</h1>
 
             <div class="form-group">
-                <textarea class="form-control" name="winter" id="winter" rows="15" cols="60"></textarea>
+                <textarea class="form-control" name="winter" id="winter" rows="15" cols="60">
+                    <?php
+                        echo $_POST['winter']
+                    ?>
+
+                </textarea>
             </div>
         </div>
 </div>
@@ -62,7 +54,12 @@ include ('model/connect.php');
 
         <div class="form-group">
 
-            <textarea class="form-control" name="spring" id="spring" rows="15" cols="60"></textarea>
+            <textarea class="form-control" name="spring" id="spring" rows="15" cols="60">
+                  <?php
+                    echo $_POST['spring']
+                  ?>
+
+            </textarea>
         </div>
     </div>
 
@@ -71,13 +68,17 @@ include ('model/connect.php');
 
         <div class="form-group">
 
-            <textarea class="form-control" name="summer" id="summer" rows="15" cols="60"></textarea>
+            <textarea class="form-control" name="summer" id="summer" rows="15" cols="60">
+                  <?php
+                    echo $_POST['summer']
+                  ?>
+
+            </textarea>
         </div>
     </div>
 </div>
 </div>
 
-<button type="submit"  value="Submit">Submit</button>
 </form>
 </div>
 
